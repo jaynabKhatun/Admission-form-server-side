060f4524a205d187256f9c239d70d4dd3c18c155
unknown <anisamim94@gmail.com>
2024-08-25 01:26:00 +0600

    added the post method $ get method

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uo3rphs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uo3rphs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,15 +24,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const studentDataCollection = client
+      .db("admissionCandidate")
+      .collection("candidates");
+
+      // post data to the database
+    app.post("/candidates", async (req, res) => {
+      const candidates = req.body;
+      console.log(candidates);
+      const result= await studentDataCollection.insertOne(candidates);
+      res.send(result);
+    });
+    
+
+    //  get data from the database
+
+    app.get("/candidates", async (req, res) => {
+      const candidates = await studentDataCollection.find().toArray();
+      res.send(candidates);
+    });
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
